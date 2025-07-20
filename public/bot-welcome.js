89845fb531025b5ca53ca336b48993f2e3f9a41f
NebulaLounge <ipotesi@indelibile.it>
2025-07-20 04:22:04 +0200

    Delete public/bot-welcome.js

--- public/bot-welcome.js
+++
@@ -1,8 +0,0 @@
-window.greetUser = function(nick) {
-  const welcomeText = `👋 Ciao ${nick}! Benvenuto su NebulaLounge 🌌`;
-  window.postMessage({
-    type: "bot-message",
-    sender: "BotNebula",
-    text: welcomeText
-  }, "*");
-}
