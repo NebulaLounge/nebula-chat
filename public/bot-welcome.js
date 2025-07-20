4e0804e8037f12ba4bee852cd3926cee2d601080
NebulaLounge <ipotesi@indelibile.it>
2025-07-20 03:52:54 +0200

    Update bot-welcome.js

--- public/bot-welcome.js
+++ public/bot-welcome.js
@@ -1,10 +1,5 @@
-export function greetUser(nick) {
+window.greetUser = function(nick) {
   const welcomeText = `👋 Ciao ${nick}! Benvenuto su NebulaLounge 🌌`;
-
-  // Log di controllo: appare nella console per confermare che il bot si attiva
-  console.log("BotNebula saluta:", welcomeText);
-
-  // Invio del messaggio al parent per essere visualizzato
   window.postMessage({
     type: "bot-message",
     sender: "BotNebula",
